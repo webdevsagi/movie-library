1df15029cbd5b14cf16f8244010d073c427601e4
Sagi Cohen <sagidevsage@gmail.com>
2025-10-31 10:48:57 +0200

    added routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\CatalogController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\MovieController;
+
+Route::get('/', [CatalogController::class, 'index'])->name('catalog.index');
+Route::get('/movie/{movie}', [CatalogController::class, 'show'])->name('catalog.show');
+
+
+
+Route::resource('movies', MovieController::class);
+
+Route::get('movies/import/search', [MovieController::class, 'importSearch'])->name('movies.import.search');
+Route::post('movies/import/find', [MovieController::class, 'importFind'])->name('movies.import.find');
+Route::post('movies/import/store', [MovieController::class, 'importStore'])->name('movies.import.store');
